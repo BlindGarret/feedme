c8d0518d1125eb231ead06dc727d4ceb0ef8ae1d
blindgarret <lroe2930@gmail.com>
2025-01-09 22:31:10 -0500

    removing extra file

--- debug.py
+++
@@ -1,8 +0,0 @@
-# just a debug wrapper for the app
-import sys
-import uvicorn
-
-sys.path.insert(0, "../app")
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
